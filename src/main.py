b954d87a19ba27f65e948d0c3ee7fbd1e46a67a8
zeziba <zeziba@mail.com>
2026-06-10 18:24:10 -0500

    Updated to use the Difficulty
    Changes to be committed:
    	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from Game import Game
+from Game import Game, Difficulty
 
 def run():
     print("Welcome to Battleship")
-    game = Game((False, False))
+    game = Game((Difficulty.MEDIUM, Difficulty.MEDIUM))
     game.set_up()
     game.take_turns()
 
