5b986b2eeb52f066ba324ad0a34174183e1ee1b0
zeziba <zeziba@mail.com>
2026-06-12 12:09:55 -0500

    Using the GameConfig as a singlton so needed a way to reset it to default state
    Changes to be committed:
    	modified:   src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -9,4 +9,9 @@ class GameConfig:
     board_height: int = GameRules.SIZE
     fleet_composition: dict = field(default_factory=lambda: GameRules.FLEET)
 
+    def reset_defaults(self):
+        self.board_height = GameRules.SIZE
+        self.board_width = GameRules.SIZE
+        self.fleet_composition = GameRules.FLEET.copy()
+
 config = GameConfig()
